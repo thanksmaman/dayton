e680232fa5438379d703ab421ca54a824e1bda26
thanksmaman <96819047+thanksmaman@users.noreply.github.com>
2022-05-19 16:38:43 +0000

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    //HERO SLIDER
+
+    $('#hero-slider').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        items:1,
+        dots:false,
+        smartSpeed:1000,
+        responsive:{
+            0:{
+
+                nav:false,
+                
+            },
+            768:{
+                nav:true,
+            }
+        }
+    })
+
+    //PROJECT SLIDERS
+
+    
+})
